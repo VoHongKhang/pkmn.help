038c618f4a6e2a2dce661ca1eb468858075dd04a
ouv1hc <Tuan.VoHung@vn.bosch.com>
2021-12-17 18:53:28 +0700

    Update vite.config.ts

--- vite.config.githubpage.ts
+++ vite.config.githubpage.ts
@@ -1,19 +1,34 @@
 import purgecss from "@fullhuman/postcss-purgecss";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { Plugin } from "postcss";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfigExport } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        purgecss({
-          content: ["./src/**/*.{ts,tsx,js}"],
-        }) as Plugin,
-      ],
+export default defineConfig((env) => {
+  const config: UserConfigExport = {
+    build: {
+      sourcemap: true,
     },
-  },
-  plugins: [reactRefresh()],
-  base: '/pkmn.help/',
+    plugins: [
+      reactRefresh(),
+      VitePWA({
+        mode: env.mode !== "development" ? "production" : "development",
+        includeAssets: ["data-pkmn.json"],
+      }),
+    ],
+    base: '/pkmn.help/',
+  };
+  if (env.mode !== "development") {
+    config.css = {
+      postcss: {
+        plugins: [
+          purgecss({
+            content: ["./index.html", "./src/**/*.{ts,tsx,js,html}"],
+          }) as Plugin,
+        ],
+      },
+    };
+  }
+  return config;
 });
